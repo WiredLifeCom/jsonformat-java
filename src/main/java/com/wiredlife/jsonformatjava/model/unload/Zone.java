4d13e6294ecce4bdb7e6e4092eb971b552621c12
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-05-20 12:48:44 +0200

    Updated zone model

--- src/main/java/com/wiredlife/jsonformatjava/model/unload/Zone.java
+++ src/main/java/com/wiredlife/jsonformatjava/model/unload/Zone.java
@@ -8,6 +8,7 @@ public class Zone {
 	private double longitude;
 	private int radius;
 	private String material;
+	private int numberOfMaterialBlocks;
 
 	private DateTime arrival;
 	private DateTime departure;
@@ -16,12 +17,13 @@ public class Zone {
 
 	}
 
-	public Zone(double latitude, double longitude, int radius, String material) {
+	public Zone(double latitude, double longitude, int radius, String material, int numberOfMaterialBlocks) {
 		this();
-		this.radius = radius;
-		this.material = material;
 		this.latitude = latitude;
 		this.longitude = longitude;
+		this.radius = radius;
+		this.material = material;
+		this.numberOfMaterialBlocks = numberOfMaterialBlocks;
 	}
 
 	public double getLatitude() {
@@ -56,6 +58,14 @@ public class Zone {
 		this.material = material;
 	}
 
+	public int getNumberOfMaterialBlocks() {
+		return numberOfMaterialBlocks;
+	}
+
+	public void setNumberOfMaterialBlocks(int numberOfMaterialBlocks) {
+		this.numberOfMaterialBlocks = numberOfMaterialBlocks;
+	}
+
 	public DateTime getArrival() {
 		return this.arrival;
 	}
@@ -76,60 +86,29 @@ public class Zone {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((this.arrival == null) ? 0 : this.arrival.hashCode());
-		result = prime * result + ((this.departure == null) ? 0 : this.departure.hashCode());
 		long temp;
-		temp = Double.doubleToLongBits(this.latitude);
+		temp = Double.doubleToLongBits(latitude);
 		result = prime * result + (int) (temp ^ (temp >>> 32));
-		temp = Double.doubleToLongBits(this.longitude);
+		temp = Double.doubleToLongBits(longitude);
 		result = prime * result + (int) (temp ^ (temp >>> 32));
-		result = prime * result + ((this.material == null) ? 0 : this.material.hashCode());
-		result = prime * result + this.radius;
 		return result;
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj) {
+		if (this == obj)
 			return true;
-		}
-		if (obj == null) {
+		if (obj == null)
 			return false;
-		}
-		if (!(obj instanceof Zone)) {
+		if (getClass() != obj.getClass())
 			return false;
-		}
 		Zone other = (Zone) obj;
-		if (this.arrival == null) {
-			if (other.arrival != null) {
-				return false;
-			}
-		} else if (!this.arrival.equals(other.arrival)) {
+		if (Double.doubleToLongBits(latitude) != Double
+				.doubleToLongBits(other.latitude))
 			return false;
-		}
-		if (this.departure == null) {
-			if (other.departure != null) {
-				return false;
-			}
-		} else if (!this.departure.equals(other.departure)) {
+		if (Double.doubleToLongBits(longitude) != Double
+				.doubleToLongBits(other.longitude))
 			return false;
-		}
-		if (Double.doubleToLongBits(this.latitude) != Double.doubleToLongBits(other.latitude)) {
-			return false;
-		}
-		if (Double.doubleToLongBits(this.longitude) != Double.doubleToLongBits(other.longitude)) {
-			return false;
-		}
-		if (this.material == null) {
-			if (other.material != null) {
-				return false;
-			}
-		} else if (!this.material.equals(other.material)) {
-			return false;
-		}
-		if (this.radius != other.radius) {
-			return false;
-		}
 		return true;
 	}
 
@@ -137,19 +116,23 @@ public class Zone {
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
 		builder.append("Zone [latitude=");
-		builder.append(this.latitude);
+		builder.append(latitude);
 		builder.append(", longitude=");
-		builder.append(this.longitude);
+		builder.append(longitude);
 		builder.append(", radius=");
-		builder.append(this.radius);
+		builder.append(radius);
 		builder.append(", material=");
-		builder.append(this.material);
+		builder.append(material);
+		builder.append(", numberOfMaterialBlocks=");
+		builder.append(numberOfMaterialBlocks);
 		builder.append(", arrival=");
-		builder.append(this.arrival);
+		builder.append(arrival);
 		builder.append(", departure=");
-		builder.append(this.departure);
+		builder.append(departure);
 		builder.append("]");
 		return builder.toString();
 	}
+	
+	
 
 }
