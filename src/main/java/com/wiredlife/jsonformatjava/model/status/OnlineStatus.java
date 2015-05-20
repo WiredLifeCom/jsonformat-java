e12acc30bc11313622262a596122f5c86019bdb1
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-05-20 19:46:37 +0200

    Added this to variables

--- src/main/java/com/wiredlife/jsonformatjava/model/status/OnlineStatus.java
+++ src/main/java/com/wiredlife/jsonformatjava/model/status/OnlineStatus.java
@@ -7,39 +7,39 @@ public class OnlineStatus {
 	private String username;
 	private boolean isHome;
 	private String ipAddress;
-	
+
 	public String getUsername() {
-		return username;
+		return this.username;
 	}
-	
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
-	
+
 	public boolean isHome() {
-		return isHome;
+		return this.isHome;
 	}
-	
+
 	public void setHome(boolean isHome) {
 		this.isHome = isHome;
 	}
-	
+
 	public String getIpAddress() {
-		return ipAddress;
+		return this.ipAddress;
 	}
-	
+
 	public void setIpAddress(String ipAddress) {
 		this.ipAddress = ipAddress;
 	}
-	
+
 	public static String toJson(OnlineStatus onlineStatus) {
 		Gson gson = new Gson();
 		return gson.toJson(onlineStatus);
 	}
-	
+
 	public static OnlineStatus fromJson(String json) {
 		Gson gson = new Gson();
 		return gson.fromJson(json, OnlineStatus.class);
 	}
-	
+
 }
