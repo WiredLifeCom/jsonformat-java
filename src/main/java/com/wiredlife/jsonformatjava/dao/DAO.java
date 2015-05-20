8b3b893ee27928f0c919f92e30abd429ab6d5707
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-05-21 00:08:43 +0200

    Added try-with-resource to statements and updated POM

--- src/main/java/com/wiredlife/jsonformatjava/dao/DAO.java
+++ src/main/java/com/wiredlife/jsonformatjava/dao/DAO.java
@@ -56,156 +56,168 @@ public class DAO {
 
 	public void addOnlineStatus(OnlineStatus onlineStatus) throws SQLException {
 		try (Connection connection = this.dataSource.getConnection()) {
-			PreparedStatement stmtSelectUser = connection.prepareStatement("SELECT UserID, Username FROM users WHERE Username=? LIMIT 1");
-			stmtSelectUser.setString(1, onlineStatus.getUsername());
-			ResultSet rsSelectUser = stmtSelectUser.executeQuery();
+			try (PreparedStatement stmtSelectUser = connection.prepareStatement("SELECT UserID, Username FROM users WHERE Username=? LIMIT 1")) {
+				stmtSelectUser.setString(1, onlineStatus.getUsername());
+				ResultSet rsSelectUser = stmtSelectUser.executeQuery();
 
-			if (!rsSelectUser.next()) {
-				PreparedStatement stmtInsertUser = connection.prepareStatement("INSERT INTO users (Username) VALUES (?)");
-				stmtInsertUser.setString(1, onlineStatus.getUsername());
-				stmtInsertUser.executeUpdate();
+				if (!rsSelectUser.next()) {
+					PreparedStatement stmtInsertUser = connection.prepareStatement("INSERT INTO users (Username) VALUES (?)");
+					stmtInsertUser.setString(1, onlineStatus.getUsername());
+					stmtInsertUser.executeUpdate();
+				}
 			}
 
 			int userID = getUserID(onlineStatus.getUsername());
 
-			PreparedStatement stmtInsertOnlineStatus = connection.prepareStatement("INSERT INTO onlinestatuses (UserID, IsHome, IpAddress) VALUES (?, ?, ?)");
-			stmtInsertOnlineStatus.setInt(1, userID);
-			stmtInsertOnlineStatus.setBoolean(2, onlineStatus.isHome());
-			stmtInsertOnlineStatus.setString(3, onlineStatus.getIpAddress());
-			stmtInsertOnlineStatus.executeUpdate();
+			try (PreparedStatement stmtInsertOnlineStatus = connection.prepareStatement("INSERT INTO onlinestatuses (UserID, IsHome, IpAddress) VALUES (?, ?, ?)")) {
+				stmtInsertOnlineStatus.setInt(1, userID);
+				stmtInsertOnlineStatus.setBoolean(2, onlineStatus.isHome());
+				stmtInsertOnlineStatus.setString(3, onlineStatus.getIpAddress());
+				stmtInsertOnlineStatus.executeUpdate();
+			}
 		}
 	}
 
 	public void addUnload(Unload unload) throws SQLException {
 		try (Connection connection = this.dataSource.getConnection()) {
-			PreparedStatement stmtSelectUser = connection.prepareStatement("SELECT UserID, Username FROM users WHERE Username=? LIMIT 1");
-			stmtSelectUser.setString(1, unload.getUser().getUsername());
-			ResultSet rsSelectUser = stmtSelectUser.executeQuery();
+			try (PreparedStatement stmtSelectUser = connection.prepareStatement("SELECT UserID, Username FROM users WHERE Username=? LIMIT 1")) {
+				stmtSelectUser.setString(1, unload.getUser().getUsername());
+				ResultSet rsSelectUser = stmtSelectUser.executeQuery();
 
-			if (!rsSelectUser.next()) {
-				PreparedStatement stmtInsertUser = connection.prepareStatement("INSERT INTO users (Username) VALUES (?)");
-				stmtInsertUser.setString(1, unload.getUser().getUsername());
-				stmtInsertUser.executeUpdate();
+				if (!rsSelectUser.next()) {
+					PreparedStatement stmtInsertUser = connection.prepareStatement("INSERT INTO users (Username) VALUES (?)");
+					stmtInsertUser.setString(1, unload.getUser().getUsername());
+					stmtInsertUser.executeUpdate();
+				}
 			}
 
 			int userID = getUserID(unload.getUser().getUsername());
 
-			PreparedStatement stmtInsertUnload = connection.prepareStatement("INSERT INTO unloads (UserID, Date) VALUES (?, ?)");
-			stmtInsertUnload.setInt(1, userID);
-			stmtInsertUnload.setString(2, unload.getUnload().toString());
-			stmtInsertUnload.executeUpdate();
+			try (PreparedStatement stmtInsertUnload = connection.prepareStatement("INSERT INTO unloads (UserID, Date) VALUES (?, ?)")) {
+				stmtInsertUnload.setInt(1, userID);
+				stmtInsertUnload.setString(2, unload.getUnload().toString());
+				stmtInsertUnload.executeUpdate();
+			}
 
 			int unloadID = getUnloadID(userID, unload.getUnload());
 
-			PreparedStatement stmtInsertUnloadsZones = connection
-					.prepareStatement("INSERT INTO unloadszones (UnloadID, Latitude, Longitude, Radius, Material, Arrival, Departure) VALUES (?, ?, ?, ?, ?, ?, ?)");
-
-			for (Zone zone : unload.getZones()) {
-				stmtInsertUnloadsZones.setInt(1, unloadID);
-				stmtInsertUnloadsZones.setDouble(2, zone.getLatitude());
-				stmtInsertUnloadsZones.setDouble(3, zone.getLongitude());
-				stmtInsertUnloadsZones.setInt(4, zone.getRadius());
-				stmtInsertUnloadsZones.setString(5, zone.getMaterial());
-				stmtInsertUnloadsZones.setString(6, zone.getArrival().toString());
-				stmtInsertUnloadsZones.setString(7, zone.getDeparture().toString());
-				stmtInsertUnloadsZones.executeUpdate();
+			try (PreparedStatement stmtInsertUnloadsZones = connection
+					.prepareStatement("INSERT INTO unloadszones (UnloadID, Latitude, Longitude, Radius, Material, Arrival, Departure) VALUES (?, ?, ?, ?, ?, ?, ?)")) {
+				for (Zone zone : unload.getZones()) {
+					stmtInsertUnloadsZones.setInt(1, unloadID);
+					stmtInsertUnloadsZones.setDouble(2, zone.getLatitude());
+					stmtInsertUnloadsZones.setDouble(3, zone.getLongitude());
+					stmtInsertUnloadsZones.setInt(4, zone.getRadius());
+					stmtInsertUnloadsZones.setString(5, zone.getMaterial());
+					stmtInsertUnloadsZones.setString(6, zone.getArrival().toString());
+					stmtInsertUnloadsZones.setString(7, zone.getDeparture().toString());
+					stmtInsertUnloadsZones.executeUpdate();
+				}
 			}
 
-			PreparedStatement stmtInsertUnloadsMaterials = connection.prepareStatement("INSERT INTO unloadsmaterials (UnloadID, Material) VALUES (?, ?)");
-			for (String material : unload.getMaterials()) {
-				stmtInsertUnloadsMaterials.setInt(1, unloadID);
-				stmtInsertUnloadsMaterials.setString(2, material);
-				stmtInsertUnloadsMaterials.executeUpdate();
+			try (PreparedStatement stmtInsertUnloadsMaterials = connection.prepareStatement("INSERT INTO unloadsmaterials (UnloadID, Material) VALUES (?, ?)")) {
+				for (String material : unload.getMaterials()) {
+					stmtInsertUnloadsMaterials.setInt(1, unloadID);
+					stmtInsertUnloadsMaterials.setString(2, material);
+					stmtInsertUnloadsMaterials.executeUpdate();
+				}
 			}
 		}
 	}
 
 	public List<Unload> getUnloads(String username) throws SQLException {
 		try (Connection connection = this.dataSource.getConnection()) {
-			PreparedStatement stmtGetUnloadIds = connection.prepareStatement("SELECT UnloadID, Date FROM unloads INNER JOIN users ON unloads.UserID = users.UserID WHERE Username=?");
-			stmtGetUnloadIds.setString(1, username);
-			ResultSet rsGetUnloadIds = stmtGetUnloadIds.executeQuery();
+			try (PreparedStatement stmtGetUnloadIds = connection.prepareStatement("SELECT UnloadID, Date FROM unloads INNER JOIN users ON unloads.UserID = users.UserID WHERE Username=?")) {
+				stmtGetUnloadIds.setString(1, username);
+				ResultSet rsGetUnloadIds = stmtGetUnloadIds.executeQuery();
 
-			List<Unload> unloads = new ArrayList<Unload>();
+				List<Unload> unloads = new ArrayList<Unload>();
 
-			while (rsGetUnloadIds.next()) {
-				Unload unload = new Unload();
+				while (rsGetUnloadIds.next()) {
+					Unload unload = new Unload();
 
-				User user = new User();
-				user.setUsername(username);
+					User user = new User();
+					user.setUsername(username);
 
-				PreparedStatement stmtGetUnloadsZones = connection.prepareStatement("SELECT Latitude, Longitude, Radius, Material, Arrival, Departure FROM unloadszones WHERE UnloadID=?");
-				stmtGetUnloadsZones.setInt(1, rsGetUnloadIds.getInt("UnloadID"));
-				ResultSet rsGetUnloadsZones = stmtGetUnloadsZones.executeQuery();
+					try (PreparedStatement stmtGetUnloadsZones = connection.prepareStatement("SELECT Latitude, Longitude, Radius, Material, Arrival, Departure FROM unloadszones WHERE UnloadID=?")) {
+						stmtGetUnloadsZones.setInt(1, rsGetUnloadIds.getInt("UnloadID"));
+						ResultSet rsGetUnloadsZones = stmtGetUnloadsZones.executeQuery();
 
-				while (rsGetUnloadsZones.next()) {
-					Zone zone = new Zone();
-					zone.setLatitude(rsGetUnloadsZones.getDouble("Latitude"));
-					zone.setLongitude(rsGetUnloadsZones.getDouble("Longitude"));
-					zone.setRadius(rsGetUnloadsZones.getInt("Radius"));
-					zone.setMaterial(rsGetUnloadsZones.getString("Material"));
-					zone.setArrival(DateTime.parse(rsGetUnloadsZones.getString("Arrival")));
-					zone.setDeparture(DateTime.parse(rsGetUnloadsZones.getString("Departure")));
+						while (rsGetUnloadsZones.next()) {
+							Zone zone = new Zone();
+							zone.setLatitude(rsGetUnloadsZones.getDouble("Latitude"));
+							zone.setLongitude(rsGetUnloadsZones.getDouble("Longitude"));
+							zone.setRadius(rsGetUnloadsZones.getInt("Radius"));
+							zone.setMaterial(rsGetUnloadsZones.getString("Material"));
+							zone.setArrival(DateTime.parse(rsGetUnloadsZones.getString("Arrival")));
+							zone.setDeparture(DateTime.parse(rsGetUnloadsZones.getString("Departure")));
 
-					unload.addZone(zone);
+							unload.addZone(zone);
+						}
+					}
+
+					try (PreparedStatement stmtGetUnloadsMaterials = connection.prepareStatement("SELECT Material FROM unloadsmaterials WHERE UnloadID=?")) {
+						stmtGetUnloadsMaterials.setInt(1, rsGetUnloadIds.getInt("UnloadID"));
+						ResultSet rsGetUnloadsMaterials = stmtGetUnloadsMaterials.executeQuery();
+
+						while (rsGetUnloadsMaterials.next()) {
+							unload.addMaterial(rsGetUnloadsMaterials.getString("Material"));
+						}
+					}
+
+					unload.setUser(user);
+					unload.setUnload(DateTime.parse(rsGetUnloadIds.getString("Date")));
+
+					unloads.add(unload);
 				}
-
-				PreparedStatement stmtGetUnloadsMaterials = connection.prepareStatement("SELECT Material FROM unloadsmaterials WHERE UnloadID=?");
-				stmtGetUnloadsMaterials.setInt(1, rsGetUnloadIds.getInt("UnloadID"));
-				ResultSet rsGetUnloadsMaterials = stmtGetUnloadsMaterials.executeQuery();
-
-				while (rsGetUnloadsMaterials.next()) {
-					unload.addMaterial(rsGetUnloadsMaterials.getString("Material"));
-				}
-
-				unload.setUser(user);
-				unload.setUnload(DateTime.parse(rsGetUnloadIds.getString("Date")));
-
-				unloads.add(unload);
+				return unloads;
 			}
-			return unloads;
 		}
 	}
 
 	public void deleteUnloads(String username) throws SQLException {
 		try (Connection connection = this.dataSource.getConnection()) {
-			PreparedStatement statement = connection.prepareStatement("DELETE FROM unloads WHERE UserID=(SELECT UserID FROM users WHERE Username=?)");
-			statement.setString(1, username);
-			statement.executeUpdate();
+			try (PreparedStatement statement = connection.prepareStatement("DELETE FROM unloads WHERE UserID=(SELECT UserID FROM users WHERE Username=?)")) {
+				statement.setString(1, username);
+				statement.executeUpdate();
+			}
 		}
 	}
 
 	public void deleteUnload(Unload unload) throws SQLException {
 		try (Connection connection = this.dataSource.getConnection()) {
-			PreparedStatement statement = connection.prepareStatement("DELETE FROM unloads WHERE UserID=(SELECT UserID FROM users WHERE Username=?) AND Date=?");
-			statement.setString(1, unload.getUser().getUsername());
-			statement.setString(2, unload.getUnload().toString());
-			statement.executeUpdate();
+			try (PreparedStatement statement = connection.prepareStatement("DELETE FROM unloads WHERE UserID=(SELECT UserID FROM users WHERE Username=?) AND Date=?")) {
+				statement.setString(1, unload.getUser().getUsername());
+				statement.setString(2, unload.getUnload().toString());
+				statement.executeUpdate();
+			}
 		}
 	}
 
 	private int getUserID(String username) throws SQLException {
 		try (Connection connection = this.dataSource.getConnection()) {
-			PreparedStatement statement = connection.prepareStatement("SELECT UserID FROM users WHERE Username=? ORDER BY UserID DESC LIMIT 1");
-			statement.setString(1, username);
-			ResultSet rs = statement.executeQuery();
-			while (rs.next()) {
-				return rs.getInt("UserID");
+			try (PreparedStatement statement = connection.prepareStatement("SELECT UserID FROM users WHERE Username=? ORDER BY UserID DESC LIMIT 1")) {
+				statement.setString(1, username);
+				ResultSet rs = statement.executeQuery();
+				while (rs.next()) {
+					return rs.getInt("UserID");
+				}
+				throw new SQLException("A user by that name doesn't exist");
 			}
-			throw new SQLException("A user by that name doesn't exist");
 		}
 	}
 
 	private int getUnloadID(int userID, DateTime dateTime) throws SQLException {
 		try (Connection connection = this.dataSource.getConnection()) {
-			PreparedStatement statement = connection.prepareStatement("SELECT UnloadID FROM unloads WHERE UserID=? AND Date=? ORDER BY UserID DESC LIMIT 1");
-			statement.setInt(1, userID);
-			statement.setString(2, dateTime.toString());
-			ResultSet rs = statement.executeQuery();
-			while (rs.next()) {
-				return rs.getInt("UnloadID");
+			try (PreparedStatement statement = connection.prepareStatement("SELECT UnloadID FROM unloads WHERE UserID=? AND Date=? ORDER BY UserID DESC LIMIT 1")) {
+				statement.setInt(1, userID);
+				statement.setString(2, dateTime.toString());
+				ResultSet rs = statement.executeQuery();
+				while (rs.next()) {
+					return rs.getInt("UnloadID");
+				}
+				throw new SQLException("A unload with that user id and date doesn't exist");
 			}
-			throw new SQLException("A unload with that user id and date doesn't exist");
 		}
 	}
 
